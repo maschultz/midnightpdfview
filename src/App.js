a48d17998415239e67e9f5e6bdaf26e3024c3b05
Michael Schultz <35410468+maschultz@users.noreply.github.com>
2022-09-17 10:33:03 -0400

    thing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import SinglePagePDFViewer from './components/pdf/single-page';
 import AllPagesPDFViewer from './components/pdf/all-pages';
-import { sampleBase64pdf } from './sampleBase64pdf';
 /* This is required only if the project file is located 
 inside the app. Otherwise you can use the external link of the pdf file*/
 import samplePDF from './MidnightWhitepaper.pdf';
@@ -12,7 +11,12 @@ import './styles.css';
 export default function App() {
 	return (
 		<div className='App'>
-			<h4>All Pages</h4>
+			<h4>
+				Midnight Whitepaper - All Pages -{' '}
+				<a href='/pdf/MidnightWhitepaper.pdf' target='_blank'>
+					Download PDF Instead
+				</a>
+			</h4>
 			<div className='all-page-container'>
 				<AllPagesPDFViewer pdf={samplePDF} />
 			</div>
