b0aefab58e656dbdfacefcf40dc97a8ed713d353
Michael Schultz <35410468+maschultz@users.noreply.github.com>
2022-09-17 10:14:29 -0400

    condense

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import SinglePagePDFViewer from "./components/pdf/single-page";
-import AllPagesPDFViewer from "./components/pdf/all-pages";
-import { sampleBase64pdf } from "./sampleBase64pdf";
+import SinglePagePDFViewer from './components/pdf/single-page';
+import AllPagesPDFViewer from './components/pdf/all-pages';
+import { sampleBase64pdf } from './sampleBase64pdf';
 /* This is required only if the project file is located 
 inside the app. Otherwise you can use the external link of the pdf file*/
-import samplePDF from "./sample.pdf";
+import samplePDF from './MidnightWhitepaper.pdf';
 
-import "./styles.css";
+import './styles.css';
 
 export default function App() {
-  return (
-    <div className="App">
-      <h4>Single Page</h4>
-      <SinglePagePDFViewer pdf={samplePDF} />
-
-      <hr />
-
-      <h4>All Pages</h4>
-      <div className="all-page-container">
-        <AllPagesPDFViewer pdf={samplePDF} />
-      </div>
-
-      <hr />
-
-      <h4>Base 64 Single Page</h4>
-      <SinglePagePDFViewer pdf={sampleBase64pdf} />
-
-      <hr />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h4>All Pages</h4>
+			<div className='all-page-container'>
+				<AllPagesPDFViewer pdf={samplePDF} />
+			</div>
+		</div>
+	);
 }
